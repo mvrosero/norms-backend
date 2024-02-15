aa70d01ec6ff03d5c97639705766a7280953e500
Michaela Vera G. Rosero <mvrosero@gbox.ncf.edu.ph>
2024-02-15 23:40:23 +0800

    latestttt

--- routes/userpage.js
+++ routes/userpage.js
@@ -13,23 +13,22 @@ router.post('/login', async (req, res) => {
     try {
         const {student_number, password } = req.body;
 
-        const getStudentQuery = 'SELECT * FROM student WHERE student_id = ?';
-        const [rows] = await db.promise().execute(getStudentQuery,[student_number]);
+        const getUserQuery = 'SELECT * FROM student WHERE student_id = ?';
+        const [rows] = await db.promise().execute(getUserQuery,[student_number]);
 
         if (rows.length === 0) {
             return res.status(401).json({ error: 'Invalid username or password' });
         }
 
-        const student = rows[0];
-        const passwordMatch = await bcrypt.compare(password, student.password);
+        const user = rows[0];
+        const passwordMatch = await bcrypt.compare(password, user.password);
 
 
         if (!passwordMatch) {
             return res.status(401).json({ error: 'Invalid username or password' });
         }
 
-
-        const token = jwt.sign({ studentID: student.id, student_number: student.student_number, }, secretKey, { expiresIn: '1h'});
+        const token = jwt.sign({userID: student.id, student_number: user.student_number }, secretKey, { expiresIn: '1h'});
 
             res.status(200).json({ token });
         }   catch (error) {
